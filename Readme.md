64c7650d6da2b8c83614f328d9e29f730a78b68f
= <=>
2022-09-06 17:15:37 +0100

    First Quizgit push -u origin master!

---
+++ Readme.md
@@ -0,0 +1,1 @@
+A Basic Quiz Web Application made by Akinsorotan Timilehin For The Cohort VI of Web3Bridge.
---
+++ js/questions.js
@@ -0,0 +1,177 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+  {
+    numb: 6,
+    question:
+      "Elements between and tags of HTML tables are ________ by default.",
+    answer: "left aligned",
+    options: ["justified", "left aligned", "center aligned", "right-aligned"],
+  },
+  {
+    numb: 7,
+    question:
+      "Which of the following tag is used intended for navigation in HTML5?",
+    answer: "nav",
+    options: ["nav", "footer", "section", "navigation tag"],
+  },
+  {
+    numb: 8,
+    question:
+      "If we don't want to allow a floating div to the left side of an element, ___________ CSS property will we use.",
+    answer: "clear",
+    options: ["margin", "float", "clear", "padding"],
+  },
+  {
+    numb: 9,
+    question:
+      "_____ CSS property allow to wrap a block of text around an image",
+    answer: "float",
+    options: ["wrap", "push", "float", "align"],
+  },
+  {
+    numb: 10,
+    question:
+      " Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+    ],
+  },
+  {
+    numb: 11,
+    question: "Which of the following tag is used to embed css in html page?",
+    answer: "style",
+    options: ["css", "!DOCTYPE html", "script", "style"],
+  },
+  {
+    numb: 12,
+    question:
+      "Which of the following CSS framework is used to create a responsive design?",
+    answer: "bootstrap",
+    options: ["django", "rails", "larawell", "bootstrap"],
+  },
+  {
+    numb: 13,
+    question:
+      "Which of the following can be used to call a JavaScript Code Snippet?",
+    answer: "Function/Method",
+    options: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+  },
+  {
+    numb: 14,
+    question:
+      "Which of the following CSS property is used to make the text bold?",
+    answer: "font-weight: bold",
+    options: [
+      "text-decoration: bold",
+      "font-weight: bold",
+      "font-style: bold",
+      "text-align: bold",
+    ],
+  },
+
+  {
+    numb: 15,
+    question:
+      "Which of the following is the property that is triggered in response to JS errors?",
+    answer: "onerror",
+    options: ["onclick", "onerror", "onmessage", "onexception"],
+  },
+  {
+    numb: 16,
+    question: "Why event handlers is needed in JS?",
+    answer: "Allows JavaScript code to alter the behaviour of windows",
+    options: [
+      "Allows JavaScript code to alter the behaviour of windows",
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences",
+    ],
+  },
+  {
+    numb: 17,
+    question:
+      "Which of the following CSS property sets the shadow for a box element?",
+    answer: "box-shadow",
+    options: ["set-shadow", "box-shadow", "shadow", "canvas-shadow"],
+  },
+  {
+    numb: 18,
+    question:
+      "Which of the following CSS Property controls how an element is positioned?",
+    answer: "position",
+    options: ["static", "position", "fix", "set"],
+  },
+  {
+    numb: 19,
+    question:
+      "Which of the following CSS selector selects the elements that are checked?",
+    answer: ":checked",
+    options: [":checked", "E ~ F", "::after", "none of the mentioned"],
+  },
+  {
+    numb: 20,
+    question:
+      "Which of the following is an appropriate value for the overflow element?",
+    answer: "all of the mentioned",
+    options: ["scroll", "hidden", "auto", "all of the mentioned"],
+  },
+];
